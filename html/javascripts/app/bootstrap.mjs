5d55463f83a2fa6f25d8efe475e394abb613207b
mini-maya <>
2026-08-18 19:57:54 +0200

    v0.0.2

--- html/javascripts/app/bootstrap.mjs
+++ html/javascripts/app/bootstrap.mjs
@@ -72,11 +72,6 @@ export function createAppBootstrap({
 		elements.folderPickerCancel.addEventListener('click', folderTargetController.closeFolderTargetDialog);
 		elements.folderPickerForm.addEventListener('submit', folderTargetController.submitFolderTargetDialog);
 		elements.folderPickerName.addEventListener('focus', folderTargetController.prepareFolderPickerNameSelection);
-		elements.folderPickerModal.addEventListener('click', function(event) {
-			if (event.target === elements.folderPickerModal) {
-				folderTargetController.closeFolderTargetDialog();
-			}
-		});
 		elements.uploadChooseButton.addEventListener('click', function(event) {
 			event.preventDefault();
 			event.stopPropagation();
@@ -120,11 +115,6 @@ export function createAppBootstrap({
 		elements.uploadDropzone.addEventListener('drop', uploadController.handleUploadDrop);
 		elements.uploadCancel.addEventListener('click', uploadController.closeUploadDialog);
 		elements.uploadConfirm.addEventListener('click', uploadController.submitUploadDialog);
-		elements.uploadModal.addEventListener('click', function(event) {
-			if (event.target === elements.uploadModal) {
-				uploadController.closeUploadDialog();
-			}
-		});
 		elements.loginCancel.addEventListener('click', function() {
 			authController.closeModal(elements.loginModal);
 		});
@@ -132,11 +122,6 @@ export function createAppBootstrap({
 			authController.submitLoginForm(event).catch(function(error) {
 				setStatus(error.message, true);
 			});
-		});
-		elements.loginModal.addEventListener('click', function(event) {
-			if (event.target === elements.loginModal) {
-				authController.closeModal(elements.loginModal);
-			}
 		});
 		elements.accountCancel.addEventListener('click', function() {
 			authController.closeModal(elements.accountModal);
@@ -146,18 +131,8 @@ export function createAppBootstrap({
 				setStatus(error.message, true);
 			});
 		});
-		elements.accountModal.addEventListener('click', function(event) {
-			if (event.target === elements.accountModal) {
-				authController.closeModal(elements.accountModal);
-			}
-		});
 		elements.adminCancel.addEventListener('click', function() {
 			authController.closeModal(elements.adminModal);
-		});
-		elements.adminModal.addEventListener('click', function(event) {
-			if (event.target === elements.adminModal) {
-				authController.closeModal(elements.adminModal);
-			}
 		});
 		elements.adminCreateGeneratePassword.addEventListener('change', function(event) {
 			elements.adminCreatePassword.disabled = event.target.checked;
